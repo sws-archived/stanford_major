1b7813936cb1d100d98744d2a5dd57cedafb7e7a
Shea McKinney <sherakama@gmail.com>
2014-09-24 14:09:58 -0700

    ios flip crash fix

--- stanford_major.js
+++ stanford_major.js
@@ -3,7 +3,7 @@
   Drupal.behaviors.flipper = {
     attach: function (context, settings) {
      $(".flip-container").click(function () {
-          $(this).toggleClass("focus");
+        $(this).toggleClass("focus");
       });
      }
    };
@@ -16,5 +16,33 @@
      }
    };
 
+   Drupal.behaviors.cantflip = {
+    attach: function (context, settings) {
+      /**
+       * IOS devices can not handle the flip
+       */
+      if(navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+        jQuery('.card-flipper', context).click(function(e) {
+          e.preventDefault();
+          var loca = $(this).find(".card-learn-more a").attr('href');
+          console.log(loca);
+          window.location = loca;
+        });
+      }
+    }
+  };
+
 
 })(jQuery);
+
+jQuery(document).ready(function() {
+
+  /**
+   * IOS devices can not handle the flip
+   */
+  if(navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+    jQuery('body').addClass('cantflip');
+  }
+
+});
+
